b8adf404fcf0590c6e5d70e4ea15de5b779be1b2
nico <nico.a.judge@gmail.com>
2023-11-18 08:50:00 -0500

    made some updates to log some more stuff

--- PartyApp/MainWindow.axaml.cs
+++ PartyApp/MainWindow.axaml.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using Avalonia.Controls;
@@ -10,7 +9,6 @@ using Avalonia.Interactivity;
 using PartyModels;
 using Serilog;
 using Serilog.Core;
-using Serilog.Sinks.SystemConsole;
 
 namespace PartyApp;
 
@@ -18,15 +16,16 @@ public partial class MainWindow : Window
 {
     // This is gonna get real big, maybe use sqlite or sql docker container instead
     private List<Message> _chatMessages = new();
-    private HttpClient HttpClient;
-    private const string _baseUrl = "http://localhost:5046";
-    private Logger logger;
+    private readonly HttpClient _httpClient;
+    private const string BaseUrl = "http://localhost:5000";
+    private readonly Logger logger;
+    
     public MainWindow()
     {
         logger = new LoggerConfiguration()
             .WriteTo.Console()
             .CreateLogger();
-        HttpClient = new HttpClient();
+        _httpClient = new HttpClient();
         Console.WriteLine("Initialized MainWindow");
         InitializeComponent();
         InitializeChatBox();
@@ -36,27 +35,32 @@ public partial class MainWindow : Window
     {
         logger.Information("InitializeChatBox called");
         ChatBox.Text = string.Empty;
-   
+        var messages = new List<Message>();
         _chatMessages.Clear();
+        try
+        {
+            var response = _httpClient.GetAsync($"{BaseUrl}/GetMessages").Result;
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                logger.Error($"Non ok status code:{response.StatusCode}, response:{response.ReasonPhrase}");
+                return;
+            }
 
-        var response = HttpClient.GetAsync($"{_baseUrl}/GetMessages").Result;
-        if (response.StatusCode != HttpStatusCode.OK)
-        {
-            logger.Error($"Non ok status code:{response.StatusCode}, response:{response.ReasonPhrase}");
-            return;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            if (responseString is null or "\"[]\"")
+            {
+                logger.Warning("No messages returned from server");
+                return;
+            }
+
+            logger.Information($"ResponseString:{responseString.ToString()}");
+            messages = JsonSerializer.Deserialize<List<Message>>(responseString);
+            _chatMessages.AddRange(messages);
         }
-      
-        
-        var responseString = response.Content.ReadAsStringAsync().Result;
-        if (responseString is null or "[]")
+        catch (Exception e)
         {
-            logger.Warning("No messages returned from server"); 
-            return;
+            logger.Error("Ran into excpetion generating chatbox:" + e.Message + " StackTrace:" + e.StackTrace);
         }
-      
-        logger.Information($"ResponseString:{responseString.ToString()}");
-        var messages = JsonSerializer.Deserialize<List<Message>>(responseString); 
-        _chatMessages.AddRange(messages); 
         logger.Information($"response was ok. Added {messages.Count} messages to the chat list");
         ChatBox.Text = MessageListToString(_chatMessages);
     }
@@ -79,7 +83,7 @@ public partial class MainWindow : Window
         MessageErrorText.IsVisible = false;
         var message = new Message(messageText, name, DateTime.Now);
         var jsonContent = new StringContent(JsonSerializer.Serialize(message));
-        var response = HttpClient.PostAsync($"{_baseUrl}/InsertMessage", jsonContent).Result;
+        var response = _httpClient.PostAsync($"{BaseUrl}/InsertMessage", jsonContent).Result;
         if (!response.IsSuccessStatusCode)
         {
             logger.Error($"response code from InsertMessage not ok:{response.StatusCode}, response:{response.ReasonPhrase}");
